4a2a7639bd6caea2683507cf255edada16b012d0
andreykatro <andrey.trofimuk@gmail.com>
2018-02-25 20:44:21 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   #root 'application#hello'
 
-  get '/help',    to: 'static_pages#help'
-  get '/about',   to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  get '/signup',  to: 'users#new'
+  get '/signup', to: 'users#new'
 
+  resources :users
 end
